65494c680b1b23b73d7623bd4cd84f274f2b7e4e
Nandini Patel <nandini_patel96@hotmail.co.uk>
2019-08-22 15:18:24 +0100

    Updates

---
+++ precache-manifest.07d621f745c6b81967e92908f202dd5a.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "2ba6c1b14d7430c122d021a81fef1e3b",
+    "url": "/nandinip-cv/static/media/swift-icon.2ba6c1b1.png"
+  },
+  {
+    "revision": "6a26c02f6b366ac8f116",
+    "url": "/nandinip-cv/static/css/main.81cc91b1.chunk.css"
+  },
+  {
+    "revision": "6f3dba8962ba9af493d4",
+    "url": "/nandinip-cv/static/js/1.6f3dba89.chunk.js"
+  },
+  {
+    "revision": "0bd920f9e8669538f202",
+    "url": "/nandinip-cv/static/js/runtime~main.0bd920f9.js"
+  },
+  {
+    "revision": "511d09468406a95e95749765a5bcee7e",
+    "url": "/nandinip-cv/static/media/collective.511d0946.png"
+  },
+  {
+    "revision": "9d2f5f920b6e9dcd024378de85f19998",
+    "url": "/nandinip-cv/static/media/sql-icon.9d2f5f92.png"
+  },
+  {
+    "revision": "22fa4e1328e352207f1266620d91a509",
+    "url": "/nandinip-cv/static/media/ruby-icon.22fa4e13.png"
+  },
+  {
+    "revision": "6a26c02f6b366ac8f116",
+    "url": "/nandinip-cv/static/js/main.6a26c02f.chunk.js"
+  },
+  {
+    "revision": "f3fb214b769cedca8837a7a3302428c6",
+    "url": "/nandinip-cv/static/media/jasmine-icon.f3fb214b.png"
+  },
+  {
+    "revision": "e7debafd0fa14c87a9b6988e9b2e769a",
+    "url": "/nandinip-cv/static/media/tdd-icon.e7debafd.png"
+  },
+  {
+    "revision": "4913e90413586105249b4f55ca622ec8",
+    "url": "/nandinip-cv/static/media/travis-icon.4913e904.png"
+  },
+  {
+    "revision": "c8f70740045c9eb55c733e975a59c1c5",
+    "url": "/nandinip-cv/static/media/nandini.c8f70740.png"
+  },
+  {
+    "revision": "7d3d597a3480a8edcd4df490e15ed657",
+    "url": "/nandinip-cv/static/media/BEBAS.7d3d597a.ttf"
+  },
+  {
+    "revision": "871d30a5f72c095d4047a09f834212b0",
+    "url": "/nandinip-cv/index.html"
+  }
+];
